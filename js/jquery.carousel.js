0e37d051d9c50433eeef7dd0e8142c5d2bf8c417
Maryna Gaponenko <maryna.a.gaponenko@gmail.com>
2016-04-17 19:18:19 +0300

    New

---
+++ js/jquery.carousel.js
@@ -0,0 +1,24 @@
+﻿(function($){
+    jQuery.fn.carousel = function(){
+	
+$(function() {
+	var $item = $('.myCarousel__item');
+	var currItem = 0;
+	$item.addClass('myCarousel__item--hide')
+		 .eq(currItem).removeClass('myCarousel__item--hide');
+	$('.myCarousel__button--left').on('click', function(){
+	    var nextItem = (currItem + 1) % $item.length;
+		$item.eq(nextItem).removeClass('myCarousel__item--hide');
+		$item.eq(currItem).addClass('myCarousel__item--hide');
+		currItem = nextItem;
+	});	
+	$('.myCarousel__button--right').on('click', function(){
+	    var nextItem = (currItem - 1 + $item.length) % $item.length;
+		$item.eq(nextItem).removeClass('myCarousel__item--hide');
+		$item.eq(currItem).addClass('myCarousel__item--hide');
+		currItem = nextItem;
+	});			
+});
+ return this;   
+};
+})(jQuery);
---
+++ template/js/script.js
@@ -0,0 +1,59 @@
+﻿'use strict'
+$(function() {
+	var headerHtml = $('#header').html(); 
+	var myData = [
+	'Гапоненко Марина Анатольевна',
+	'https://vk.com/id5548230',
+	'./img/mylogo.jpg',
+	'Изучаю frontend в GoIt. Окончила курсы QA.'
+	];
+	
+	var headerContent = tmpl(headerHtml, { 
+	data: myData  
+	});
+
+	$('body').append(headerContent);
+	
+	var learnHtml = $('#learn').html(); 
+	var myLearn = [
+	{
+	list: 'Мне это интересно'
+	},
+	{
+	list: 'Востребованная проффесия'
+	},
+	{
+	list: 'Постоянное развитие вместе с IT сферой'
+	}
+	];
+	
+	var learnContent = tmpl(learnHtml, { 
+	data1: myLearn  
+	});
+
+	$('.centre').append(learnContent);
+	
+	var contactsHtml = $('#contacts').html(); 
+	var myContacts = [
+	'+380954961781',
+	'https://vk.com/id5548230',
+	'vk.com'
+	];
+	
+	var contactsContent = tmpl(contactsHtml, { 
+	data2: myContacts  
+	});
+
+	$('.centre').append(contactsContent);
+	
+	var feedbackHtml = $('#feedback').html(); 
+	var myFeedback = [
+	'Приложу все усилия чтобы получить по максимальному знания на курсе'
+	];
+	
+	var feedbackContent = tmpl(feedbackHtml, { 
+	data3: myFeedback  
+	});
+
+	$('.centre').append(feedbackContent);
+});	
